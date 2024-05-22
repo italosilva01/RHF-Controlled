774176daa0eeb7638fa90c0cf003592aa6c24a59
Francisco Italo <italo.silvan87@gmail.com>
2024-05-22 16:07:14 -0300

    add jest and testingLibrary

---
+++ src/pages/result/result.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+import Result from ".";
+
+describe("Result", () => {
+  test("renders without crashing", () => {
+    render(<Result />);
+    expect(
+      screen.getByText("Este é o preço de compra do veículo")
+    ).toBeInTheDocument();
+  });
+});
